081536c5d973e8a48dafd6fd6d0587683e64484b
danbi <leedanbiii@naver.com>
2023-12-07 13:42:55 +0900

    프로젝트 생성

---
+++ src/main/java/com/wimpnew/WimpNewApplication.java
@@ -0,0 +1,13 @@
+package com.wimpnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WimpNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WimpNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wimpnew/WimpNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimpnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WimpNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
